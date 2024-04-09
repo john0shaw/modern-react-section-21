916f6e253a1ebccc895ccc9e8be21200cf45100f
John Shaw <john0shaw@gmail.com>
2024-04-08 19:19:00 -0700

    Completed Section 22 - implemented photos in album

--- src/store/index.js
+++ src/store/index.js
@@ -3,16 +3,19 @@ import { setupListeners } from "@reduxjs/toolkit/query";
 import { usersReducer } from "./slices/usersSlice";
 
 import { albumsApi } from "./apis/albumsApi";
+import { photosApi } from "./apis/photosApi";
 
 
 export const store = configureStore({
     reducer: {
         users: usersReducer,
-        [albumsApi.reducerPath]: albumsApi.reducer
+        [albumsApi.reducerPath]: albumsApi.reducer,
+        [photosApi.reducerPath]: photosApi.reducer
     },
     middleware: (getDefaultMiddleware) => {
         return getDefaultMiddleware()
-            .concat(albumsApi.middleware);
+            .concat(albumsApi.middleware)
+            .concat(photosApi.middleware);
     }
 });
 
@@ -21,8 +24,15 @@ setupListeners(store.dispatch);
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/removeUser';
+
 export {
     useFetchAlbumsQuery,
     useAddAlbumMutation,
     useRemoveAlbumMutation
 } from './apis/albumsApi';
+
+export {
+    useFetchPhotosQuery,
+    useAddPhotoMutation,
+    useRemovePhotoMutation
+} from './apis/photosApi';
